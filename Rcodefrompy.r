9259d0f1e8217c82e46f087f2fcd55a5d9807c90
Nathan A. Paxton <napaxton@gmail.com>
2017-11-09 18:25:44 -0500

    Some changes to deal with pandas updates, primarily on .sort methods

--- Rcodefrompy.r
+++ Rcodefrompy.r
@@ -1,7 +1,7 @@
 library(sensitivity); library(tidyverse); library(dplyr)
 
 setwd("~/GitHub/maine_infant_mortality_data")
-infmort <- read.delim("./data/us_infant_mortality_2.tsv", check.names = F)
+infmort <- read_tsv("./data/us_infant_mortality_2.tsv")
 
 OAT <- function(infmort, USstatecase, howmuch = 1){  # function(frame, case, howmuch, sort.cols = NULL, sort.asc = NULL){
   # if (is.null(sort.cols) = F )
@@ -18,33 +18,40 @@ OAT <- function(infmort, USstatecase, howmuch = 1){  # function(frame, case, how
   #
   infmort <- infmort %>%
     gather(datencat, measure, 2:length(infmort)) %>%
-    separate(datencat, c("year","infant"), sep = "_") %>%
+    separate(datencat, c("year","infant"), sep = "_", convert = T) %>%
     spread(infant, measure) %>%
     rename(state = State, births = Births, deaths = Deaths)
   
   # create a list of the years with 'state' at the initial position
+  ## R, make a df with state years as the UoA
+#  out.rates <- data.frame(infmort$state, infmort$year)
   
-  # make a list of 'rates'
+  # ✔ make a list of 'rates'
   
-  # states as list drawn from the 'index' index in the raw/infmort object/structure
+  # ✔ states as list drawn from the 'index' index in the raw/infmort object/structure
   
   # For each state-year calculate 
     # birth value
       # get birth-value
-      b <- # for the state passed in, grab its raw births
+      # b <- # for the state passed in, grab its raw births
+    
     # death value
       #get death value
-      d <- # grab the raw deaths and add the 'howmuch' parameter
+      # d <- # grab the raw deaths and add the 'howmuch' parameter
     # d = 0 if the d in the infmort set is <0 (so if stmt above)
-  
+ infmort %>%
+          mutate(rate = (deaths + howmuch)/births) 
+      # Error: Error in mutate_impl(.data, dots) : 
+      # Evaluation error: non-numeric argument to binary operator.
+      
   # calculate the value of d/b, then place it in a cell appropriate to that year
   # if more than one state, create a new row for each state (where columns represent years of the IMR)
-#return a dataframe containing the row, column of state-years
+#return a formatted dataframe containing the row, column of state-years
   
+# rank the  numerical data ranks (1 through n) among the states (byt the states column) for each year; python's axis = 0 means rank by the column, while axis = 1 means rank by the row). "A 2-dimensional array has two corresponding axes: the first running vertically downwards across rows (axis 0), and the second running horizontally across columns (axis 1)." https://docs.scipy.org/doc/numpy-1.10.0/glossary.html
+          
 }
 
-# library(psych)
 # 
-# cohen.d()
 
 raterank <- OAT(infmort = infmort, "Maine", howmuch = 1)
